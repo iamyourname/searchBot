b3b256206b8054d38ceee43297a41e56ce3dfda1
iamyourname <52793743+iamyourname@users.noreply.github.com>
2025-03-04 19:13:06 +0300

    Update Connections.java

--- src/main/java/org/example/Connections.java
+++ src/main/java/org/example/Connections.java
@@ -1,6 +1,23 @@
 public class Connections {
 
+
+final public static String baseDriver = "org.postgresql.Driver";
+    public static Connection connLog;
+
+    public static String urlLog = "jdbc:postgresql://basis-supp.database-test-pg.cloud.vimpelcom.ru:5432/poweruser";
 public static void getConnection(){
+
+public static void initConnection() throws SQLException, ClassNotFoundException {
+        try {
+            logger.info("TRY TO INIT AND CONNECT LOG DATABASE");
+            Class.forName(baseDriver);
+            connLog = DriverManager.getConnection(urlLog, "mivyamoiseev", "xid123MTPP_");
+            connLog.setAutoCommit(false);
+            logger.info("LOG DATABASE...OK");
+        } catch (SQLException e) {
+            logger.error("FAILED TO CONNECT LOG DATABASE: "+e.toString());
+        }
+    }
 
 }
 
